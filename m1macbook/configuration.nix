8ed727cbf6ae0c290f1d45e7d191d019ca582ec3
Mikey O'Brien <hmobrienv@gmail.com>
2021-09-02 09:13:44 -0500

    working m1macbook config

--- m1macbook/configuration.nix
+++ m1macbook/configuration.nix
@@ -1,15 +1,27 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, home-manager, ... }:
 
 {
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = lib.optionalString (config.nix.package == pkgs.nixFlakes)
+    "experimental-features = nix-command flakes";
+
+  users.users.mikeyobrien = {
+    name = "mikeyobrien";
+    home = "/Users/mikeyobrien";
+  };
+
+  home-manager = {
+    useUserPackages = true;
+    useGlobalPkgs = true;
+    users.mikeyobrien = import ./home.nix;
+  };
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages =
     [ pkgs.vim
     ];
 
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
   # Auto upgrade nix package and the daemon service.
   # services.nix-daemon.enable = true;
@@ -18,7 +30,7 @@
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
   # programs.fish.enable = true;
-  environment.darwinConfig = "$HOME/.config/nix-darwin/m1macbook/configuration.nix";
+  # environment.darwinConfig = "$HOME/.config/nix-darwin/m1macbook/configuration.nix";
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
