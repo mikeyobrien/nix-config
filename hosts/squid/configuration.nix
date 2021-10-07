149acd521fb7e57785b0ea70fa88bc9cda8b439f
hmobrienv@gmail.com <hmobrienv@gmail.com>
2021-10-07 16:23:07 +0000

    update configuration

---
+++ hosts/squid/configuration.nix
@@ -0,0 +1,65 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.ens33.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.displayManager = {
+    defaultSession = "none+bspwm";
+    lightdm.enable = true;
+    lightdm.greeters.mini.enable = true;
+  };
+  services.xserver.windowManager.bspwm.enable = true;
+    
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.root.initialPassword = "nixos";
+  users.users.mikeyobrien = {
+    isNormalUser = true;
+    initialPassword = "nixos"
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = with pkgs; [
+    vim 
+    wget
+    firefox
+  ];
+
+  services.openssh.enable = true;
+
+  system.stateVersion = "21.05"; 
+}
+
