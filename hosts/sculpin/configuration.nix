85171d378d4d00a7ff0240a8c2222b8d4ef6f70b
Mikey O'Brien <hmobrienv@gmail.com>
2021-10-16 10:16:49 -0500

    add open-vm-tools

--- hosts/sculpin/configuration.nix
+++ hosts/sculpin/configuration.nix
@@ -38,10 +38,40 @@ with lib;
   environment.systemPackages = with pkgs; [
     vim 
     wget
-    # open-vm-tools
+    open-vm-tools
     git
   ];
 
+
+   systemd.services.vmware =
+      { description = "VMWare Guest Service";
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig.ExecStart = "${open-vm-tools}/bin/vmtoolsd";
+      };
+
+    # Mount the vmblock for drag-and-drop and copy-and-paste.
+    systemd.mounts = [
+      {
+        description = "VMware vmblock fuse mount";
+        documentation = [ "https://github.com/vmware/open-vm-tools/blob/master/open-vm-tools/vmblock-fuse/design.txt" ];
+        before = [ "vmware.service" ];
+        wants = [ "vmware.service" ];
+        what = "${open-vm-tools}/bin/vmware-vmblock-fuse";
+        where = "/run/vmblock-fuse";
+        type = "fuse";
+        options = "subtype=vmware-vmblock,default_permissions,allow_other";
+        wantedBy = [ "multi-user.target" ];
+      }
+    ];
+
+    security.wrappers.vmware-user-suid-wrapper =
+      { setuid = true;
+        owner = "root";
+        group = "root";
+        source = "${open-vm-tools}/bin/vmware-user-suid-wrapper";
+      };
+
+    environment.etc.vmware-tools.source = "${open-vm-tools}/etc/vmware-tools/*";
   services.openssh.enable = true;
   networking.firewall.enable = false;
 
