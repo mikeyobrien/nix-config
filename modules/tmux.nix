7b9ee3ca54bf89541b53d0ae40ad3e688424178b
Mikey O'Brien <hmobrienv@gmail.com>
2021-12-06 09:52:29 -0600

    fix tmux vi-copy-mode

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -16,12 +16,21 @@ in {
     homeManagerPrograms.tmux = {
       enable = true;
       terminal = "screen-256color";
+      escapeTime = 0;
       shortcut = "a";
       keyMode = "vi";
       baseIndex = 1;
       aggressiveResize = true;
       plugins = with pkgs; [
-        tmuxPlugins.yank
+        {
+          plugin = tmuxPlugins.yank;
+          extraConfig = '' 
+            set -g @yank_selection 'primary'
+            bind-key -T copy-mode-vi 'v' send -X begin-selection
+            bind-key -T copy-mode-vi 'r' send -X rectangle-toggle
+            bind-key -T copy-mode-vi 'y' send -X copy-pipe-and-cancel
+          '';
+        }
         tmuxPlugins.vim-tmux-navigator
         tmuxPlugins.power-theme
       ];
